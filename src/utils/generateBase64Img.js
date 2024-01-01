9e665eccbc7586f3d73360a7e7d8724a1d724d51
Shahzaib Ahmed <Shahzaib.ahmed@aivacreative.com>
2024-01-01 20:01:34 +0500

    Testing

--- src/utils/generateBase64Img.js
+++ src/utils/generateBase64Img.js
@@ -2,7 +2,7 @@ const DatauriParser = require('datauri/parser');
 
 const parser = new DatauriParser();
 
-//Parse Image to BASE 64
+//Parse Image to BASE 64 
 const bufferToDataURI = (fileFormat, buffer) => parser.format(fileFormat, buffer);
 
 module.exports = {
